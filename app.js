e24dcb539dab0e9f5452cbea6e3b78c2a6a3d8ef
Ben Radcliffe-Brown <benradcliffe-brown@bens-air.home>
2024-09-03 20:32:58 +0100

    Updates

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
 const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
-        console.log(entry)
         if (entry.isIntersecting) {
             entry.target.classList.add('show');
-        }   else {
+            entry.target.setAttribute('aria-hidden', 'false');
+        } else {
             entry.target.classList.remove('show');
+            entry.target.setAttribute('aria-hidden', 'true');
         }
-    })
+    });
+}, {
+    threshold: 0.1
 });
 
 const hiddenElements = document.querySelectorAll('.hidden');
 
-hiddenElements.forEach((el) => observer.observe(el));
+hiddenElements.forEach((el) => {
+    el.setAttribute('aria-hidden', 'true');
+    observer.observe(el);
+});

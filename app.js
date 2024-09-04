2dfd16ac4d3fbb4d3ab794700dc9fed896ca6b7f
bradcliffebrown <ben.radcliffebrown@outlook.com>
2024-09-04 18:52:49 +0100

    additional scroller

--- app.js
+++ app.js
@@ -18,3 +18,16 @@ hiddenElements.forEach((el) => {
     el.setAttribute('aria-hidden', 'true');
     observer.observe(el);
 });
+
+window.addEventListener('scroll', function() {
+    const header = document.getElementById('header');
+    const h1 = header.querySelector('h1');
+
+    if (window.scrollY > 50) { // Adjust the scroll threshold as needed
+        header.style.backgroundColor = 'rgba(44, 62, 80, 0)'; // Make background transparent
+        h1.style.fontSize = '1.7em'; // Reduce font size by 15%
+    } else {
+        header.style.backgroundColor = 'rgba(44, 62, 80, 0.8)'; // Restore background
+        h1.style.fontSize = '2em'; // Restore original font size
+    }
+});
